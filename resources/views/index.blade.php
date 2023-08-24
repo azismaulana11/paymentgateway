ccf1c62542c0446dc855a54857bdd60097eb7150
azismaulana11 <azism145@gmail.com>
2023-08-24 13:10:58 +0700

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{ url('assets/style.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+</head>
+<body>
+  <form action="/payment" method="GET">
+    <h1>Data diri</h1>
+    <div class="formcontainer">
+    <hr/>
+    <div class="container">
+      <label for="nama"><strong>Nama</strong></label>
+      <input type="text" placeholder="Masukkan Nama" name="nama" required>
+      <label for="email"><strong>Email</strong></label>
+      <input type="text" placeholder="Masukkan email" name="email" required>
+      <label for="number"><strong>Nomor</strong></label>
+      <input type="text" placeholder="Masukkan email" name="number" required>
+    </div>
+    <button type="submit">Lanjut</button>
+  </form>
+  @if(session('alert-success'))
+  <script>alert("{{ session('alert-success') }}")</script>
+  @elseif(session('alert-failed'))
+  <script>alert("{{ session('alert-failed') }}")</script>
+  @endif
+</body>
+</html>
